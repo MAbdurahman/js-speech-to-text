08e6de5456ca8f85f5f79d5b7f42a5d1cf5d2ce8
MAbdurahman <mdbdrrhm@gmail.com>
2023-03-17 04:53:59 -0500

    Add preloader gif, its styles and script

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 
+/* ============================================
+            preloader
+===============================================*/
+$(window).on('load', function () {
+  // makes sure that whole site is loaded
+  $('#preloader-gif, #preloader').fadeOut(3000, function () {});
+});
 
 
 
@@ -7,6 +14,13 @@
 $(document).ready(function () {
   //**************** variables ****************//
   const input_language = document.querySelector("#language");
+  const clear_button = document.querySelector("#clear-button");
+  const download_button = document.querySelector("#download-button");
+  const record_button = document.querySelector("#record-button");
+  
+  let SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+  let recording = false;
+  let recognition;
   
   
   /**
@@ -22,6 +36,41 @@ $(document).ready(function () {
     
   }// end of populateLanguages function
   
+  /**
+   * @description -
+   */
+  function clearTextField() {
+  
+  }//end of the clearTextField function
+  
+  /**
+   * @description -
+   */
+  function downloadText() {
+  
+  }// end of the downloadText function
+  
+  function startOrStopRecording() {}
+  
+  /**
+   * @description -
+   */
+  function speechToText() {
+  
+  }//end of the speechToText function
+  
+  /**
+   * @description -
+   */
+  function stopRecording() {
+  
+  }//end of the stopRecording function
+  
   populateLanguages();
+  
+  
+  clear_button.addEventListener('click', clearTextField);
+  download_button.addEventListener('click', downloadText);
+  record_button.addEventListener('click', startOrStopRecording);
   
 });
